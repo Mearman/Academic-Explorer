129ecc51d1b58ed975a6261413842e0910f0c44b
Joseph Mearman <joseph@mearman.co.uk>
2025-08-15 07:54:30 +0100

    feat: integrate AppShellLayout into application
    Wire up the new AppShellLayout template as the main application
    shell in the root route and export new components from index.
    This provides consistent navigation and layout structure.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { AppShellLayout } from '../components/templates/app-shell-layout';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <AppShellLayout>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </AppShellLayout>
   ),
 });
