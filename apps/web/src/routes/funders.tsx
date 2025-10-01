02d1e5fb862c86272a6733134edab47ddf2b06ff
Joseph Mearman <joseph@mearman.co.uk>
2025-10-01 07:37:24 +0100

    feat: add funders route with EntityList
    Add /funders route that displays a list of funders using the
    EntityList component with columns for ID, name, country code,
    and international status.

---
+++ apps/web/src/routes/funders.tsx
@@ -0,0 +1,17 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { EntityList, type ColumnConfig } from '@/components/EntityList';
+
+export const Route = createFileRoute('/funders')({
+  component: FundersRoute,
+});
+
+const fundersColumns: ColumnConfig[] = [
+  { key: 'id', header: 'ID' },
+  { key: 'display_name', header: 'Name' },
+  { key: 'country_code', header: 'Country' },
+  { key: 'international', header: 'International' },
+];
+
+function FundersRoute() {
+  return <EntityList entityType="funders" columns={fundersColumns} title="Funders" />;
+}
