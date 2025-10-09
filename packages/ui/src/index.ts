74fdb6b214e5e2d932d0d7af9f31d296e0e1813f
Joseph Mearman <joseph@mearman.co.uk>
2025-10-09 19:50:45 +0100

    feat(ui-components): export entity card components from ui package
    - export all card components and grid
    - export Cards namespace with all card types
    - make cards available to consuming applications
    - complete entity card implementation

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -20,6 +20,28 @@ export {
 export { RichEntityView, FieldRenderer } from "./components/entity-views";
 export { ViewToggle, type ViewToggleProps } from "./components/ViewToggle";
 
+// Entity Card Components
+export {
+  EntityCard,
+  type EntityCardProps,
+  WorkCard,
+  type WorkCardProps,
+  AuthorCard,
+  type AuthorCardProps,
+  SourceCard,
+  type SourceCardProps,
+  InstitutionCard,
+  type InstitutionCardProps,
+  TopicCard,
+  type TopicCardProps,
+  PublisherCard,
+  type PublisherCardProps,
+  FunderCard,
+  type FunderCardProps,
+  EntityCardGrid,
+  type EntityCardGridProps,
+} from "./components/cards";
+
 // Types
 export * from "./types/common";
 
@@ -27,3 +49,4 @@ export * from "./types/common";
 export * as DataDisplay from "./components/data-display";
 export * as Layout from "./components/layout";
 export * as Feedback from "./components/feedback";
+export * as Cards from "./components/cards";
