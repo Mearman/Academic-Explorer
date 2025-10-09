af3bc2dd12c9f355528f287be19a922a2ee92073
Joseph Mearman <joseph@mearman.co.uk>
2025-10-09 20:31:58 +0100

    style(ui-components): format entity card exports
    - remove trailing commas from export statements
    - alphabetize card component exports
    - alphabetize namespace exports
    - maintain consistent formatting across file

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,52 +1,36 @@
 // Data Display Components
 export {
   BaseTable,
-  type BaseTableProps,
+  type BaseTableProps
 } from "./components/data-display/BaseTable";
 
 // Layout Components
 export {
   CollapsibleSection,
-  type CollapsibleSectionProps,
+  type CollapsibleSectionProps
 } from "./components/layout/CollapsibleSection";
 
 // Feedback Components
 export {
   ErrorBoundary,
-  type ErrorBoundaryProps,
+  type ErrorBoundaryProps
 } from "./components/feedback/ErrorBoundary";
 
 // Entity View Components
-export { RichEntityView, FieldRenderer } from "./components/entity-views";
+export { FieldRenderer, RichEntityView } from "./components/entity-views";
 export { ViewToggle, type ViewToggleProps } from "./components/ViewToggle";
 
 // Entity Card Components
 export {
-  EntityCard,
-  type EntityCardProps,
-  WorkCard,
-  type WorkCardProps,
-  AuthorCard,
-  type AuthorCardProps,
-  SourceCard,
-  type SourceCardProps,
-  InstitutionCard,
-  type InstitutionCardProps,
-  TopicCard,
-  type TopicCardProps,
-  PublisherCard,
-  type PublisherCardProps,
-  FunderCard,
-  type FunderCardProps,
-  EntityCardGrid,
-  type EntityCardGridProps,
+  AuthorCard, EntityCard, EntityCardGrid, FunderCard, InstitutionCard, PublisherCard, SourceCard, TopicCard, WorkCard, type AuthorCardProps, type EntityCardGridProps, type EntityCardProps, type FunderCardProps, type InstitutionCardProps, type PublisherCardProps, type SourceCardProps, type TopicCardProps, type WorkCardProps
 } from "./components/cards";
 
 // Types
 export * from "./types/common";
 
 // Component category exports for tree-shaking
-export * as DataDisplay from "./components/data-display";
-export * as Layout from "./components/layout";
-export * as Feedback from "./components/feedback";
 export * as Cards from "./components/cards";
+export * as DataDisplay from "./components/data-display";
+export * as Feedback from "./components/feedback";
+export * as Layout from "./components/layout";
+
