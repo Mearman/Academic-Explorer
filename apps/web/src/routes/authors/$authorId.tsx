8a0f4239bf1de1808238f09aa01ff009f755b7c4
Joseph Mearman <joseph@mearman.co.uk>
2025-10-28 00:03:33 +0000

    chore: attempt to fix React Hook #311 error by removing lazy wrapper
    - Removed lazy loading and Suspense wrapper from author route
    - This is an attempt to resolve hook ordering issues
    - Error pre-existed these changes (confirmed via git bisect)
    - Issue affects author route but not home page
    - Related to React 19 hook tracking system

--- apps/web/src/routes/authors/$authorId.tsx
+++ apps/web/src/routes/authors/$authorId.tsx
@@ -1,18 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { lazy } from "react";
-import { LazyRoute } from "@/components/routing/LazyRoute";
 import { z } from "zod";
-
-const AuthorRoute = lazy(() =>
-  import("./$authorId.lazy").then((m) => ({ default: m.default })),
-);
+import AuthorRoute from "./$authorId.lazy";
 
 export const Route = createFileRoute("/authors/$authorId")({
-  component: () => (
-    <LazyRoute>
-      <AuthorRoute />
-    </LazyRoute>
-  ),
+  component: AuthorRoute,
   validateSearch: z.object({
     select: z.string().optional(),
   }),
